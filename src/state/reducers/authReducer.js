8cf0b0513b169d16aec68a3562426a33c4a09f6c
oyekunle-mark <oyekunlemac@gmail.com>
2019-06-23 21:36:25 +0100

    The reducer to handle user authentication

---
+++ src/state/reducers/authReducer.js
@@ -0,0 +1,30 @@
+import actionTypes from '../actionTypes';
+
+const initialState = {
+  users: [],
+  user: {},
+  registering: false,
+  loginIn: false,
+  gettingUsers: false,
+};
+
+const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.REGISTER:
+      return { ...state, registering: false };
+    case actionTypes.REGISTERING:
+      return { ...state, registering: true };
+    case actionTypes.LOGIN:
+      return { ...state, user: action.payload.user, loginIn: false };
+    case actionTypes.LOGIN_IN:
+      return { ...state, loginIn: true };
+    case actionTypes.GET_USERS:
+      return { ...state, users: action.payload, gettingUsers: false };
+    case actionTypes.GETTING_USERS:
+      return { ...state, gettingUsers: true };
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
