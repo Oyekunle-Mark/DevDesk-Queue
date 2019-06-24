c558dd8a80bf7bea01586380025b1223c97a0b54
oyekunle-mark <oyekunlemac@gmail.com>
2019-06-24 11:24:20 +0100

    Setup the authentication component in the app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import LandingPage from './components/LandingPage';
+import Registration from './components/Registration';
+import Login from './components/Login';
 
 const App = () => (
-  <div>
-    <h1>Hello, World!</h1>
-  </div>
+  <Router>
+    <div>
+      <Route exact path="/" component={LandingPage} />
+      <Route path="/join" render={props => <Registration {...props} />} />
+      <Route path="/login" render={props => <Login {...props} />} />
+    </div>
+  </Router>
 );
 
 export default App;
