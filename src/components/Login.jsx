478dd593241bfa3bbb89874a8762b9f0256c086f
oyekunle-mark <oyekunlemac@gmail.com>
2019-06-24 14:02:40 +0100

    Improved returned the res anderr of the axios call

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,37 +1,45 @@
 import React, { createRef } from 'react';
-import { func, bool } from 'prop-types';
+import { func, bool, object } from 'prop-types';
 import { connect } from 'react-redux';
 
 import { login } from '../state/actionCreators';
 
-const Login = ({ login, loginIn }) => {
+const Login = ({ login, loginIn, history, error }) => {
   const username = createRef();
   const password = createRef();
 
   const handleLogin = e => {
     e.preventDefault();
 
-    login(username.current.value, password.current.value);
+    login(username.current.value, password.current.value).then(res => {
+      if (res.status === 200) history.push('/home');
+    });
   };
 
   return (
-    <form onSubmit={handleLogin}>
-      <h3>Login</h3>
+    <div>
+      <form onSubmit={handleLogin}>
+        <h3>Login</h3>
 
-      <input type="text" placeholder="username" ref={username} />
-      <input type="password" placeholder="password" ref={password} />
-      <button type="submit">Login {loginIn && 'Loading...'}</button>
-    </form>
+        <input type="text" placeholder="username" ref={username} />
+        <input type="password" placeholder="password" ref={password} />
+        <button type="submit">Login {loginIn && 'Loading...'}</button>
+      </form>
+      {error && <h4>Error</h4>}
+    </div>
   );
 };
 
 Login.propTypes = {
   login: func.isRequired,
   loginIn: bool.isRequired,
+  history: object.isRequired,
+  error: bool.isRequired,
 };
 
 const mapStateToProps = ({ user }) => ({
   loginIn: user.loginIn,
+  error: user.error,
 });
 
 export default connect(
