01968eb63ec67b8f37a3a326454fa69293f8bc6e
oyekunle-mark <oyekunlemac@gmail.com>
2019-06-24 11:32:01 +0100

    Imported and used the middleware to add token to the local storage

--- index.jsx
+++ index.jsx
@@ -7,11 +7,12 @@ import logger from 'redux-logger';
 
 import App from './src/App';
 import rootReducer from './src/state/reducers';
+import addTokenToLocalStorage from './src/state/middlewares';
 
 const store = createStore(
   rootReducer,
   compose(
-    applyMiddleware(thunk, logger),
+    applyMiddleware(thunk, logger, addTokenToLocalStorage),
     window.__REDUX_DEVTOOLS_EXTENSION__ &&
       window.__REDUX_DEVTOOLS_EXTENSION__(),
   ),
