edca8b9663063d74e3fa48eb75b9e5a0b9c6aa20
oyekunle-mark <oyekunlemac@gmail.com>
2019-06-25 20:03:26 +0100

    Added a 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import LandingPage from './components/LandingPage';
 import Registration from './components/Registration';
@@ -10,19 +10,27 @@ import CreateTicket from './components/studentComponents/CreateTicket';
 import MyTickets from './components/studentComponents/MyTickets';
 import HelperHome from './components/helperComponents/HelperHome';
 import ManageTickets from './components/helperComponents/ManageTickets';
+import NoMatchPage from './components/noMatchPage';
 
 const App = () => (
   <Router>
     <div>
-      <Route exact path="/" component={LandingPage} />
-      <Route path="/join" render={props => <Registration {...props} />} />
-      <Route path="/login" render={props => <Login {...props} />} />
-      <WithAuth path="/home" component={StudentHome} />
-      <WithAuth path="/create_ticket" component={CreateTicket} />
-      <WithAuth path="/my_tickets" component={MyTickets} />
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route
+          exact
+          path="/join"
+          render={props => <Registration {...props} />}
+        />
+        <Route exact path="/login" render={props => <Login {...props} />} />
+        <WithAuth exact path="/home" component={StudentHome} />
+        <WithAuth exact path="/create_ticket" component={CreateTicket} />
+        <WithAuth exact path="/my_tickets" component={MyTickets} />
 
-      <WithAuth path="/helper" component={HelperHome} />
-      <WithAuth path="/manage_tickets" component={ManageTickets} />
+        <WithAuth exact path="/helper" component={HelperHome} />
+        <WithAuth exact path="/manage_tickets" component={ManageTickets} />
+        <Route component={NoMatchPage} />
+      </Switch>
     </div>
   </Router>
 );
