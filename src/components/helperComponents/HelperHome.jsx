2c66a98a17ba9a0abd13a5ce670d15b391b55697
oyekunle-mark <oyekunlemac@gmail.com>
2019-06-25 16:16:53 +0100

    Helper users home

---
+++ src/components/helperComponents/HelperHome.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect } from 'react';
+import { func, arrayOf, object, bool } from 'prop-types';
+import { connect } from 'react-redux';
+
+import { getTickets, updateHelperTicket } from '../../state/actionCreators';
+
+import HelperNav from './HelperNav';
+import HelperTicket from './HelperTicket';
+
+const HelperHome = ({
+  getTickets,
+  tickets,
+  gettingTickets,
+  updateHelperTicket,
+}) => {
+  useEffect(getTickets, []);
+
+  const userId = JSON.parse(localStorage.getItem('DevDeskAuth')).user.user_id;
+
+  const ticketList = tickets.map(ticket => {
+    if (ticket.resolved === 0 && ticket.assigned_user !== userId)
+      return (
+        <HelperTicket
+          key={ticket.id}
+          {...ticket}
+          helper
+          assignToSelf={updateHelperTicket}
+        />
+      );
+  });
+
+  return (
+    <div>
+      <HelperNav />
+      {gettingTickets && <h4>Loading</h4>}
+      {ticketList}
+    </div>
+  );
+};
+
+HelperHome.propTypes = {
+  getTickets: func.isRequired,
+  tickets: arrayOf(object).isRequired,
+  gettingTickets: bool.isRequired,
+  updateHelperTicket: func.isRequired,
+};
+
+const mapStateToProps = ({ ticket }) => ({
+  tickets: ticket.tickets,
+  gettingTickets: ticket.gettingTickets,
+});
+
+export default connect(
+  mapStateToProps,
+  { getTickets, updateHelperTicket },
+)(HelperHome);
