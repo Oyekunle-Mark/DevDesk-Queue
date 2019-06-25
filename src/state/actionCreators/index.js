3ab8966af862b118c33997094642839d67d54f90
oyekunle-mark <oyekunlemac@gmail.com>
2019-06-25 16:21:17 +0100

    Added a new action type for helper operations

--- src/state/actionCreators/index.js
+++ src/state/actionCreators/index.js
@@ -136,6 +136,31 @@ export const updateTicket = (id, title, description, category) => dispatch => {
     });
 };
 
+export const updateHelperTicket = (
+  id,
+  resolved,
+  assigned,
+  assigned_user,
+) => dispatch => {
+  dispatch({ type: actionTypes.UPDATING_TICKET });
+
+  return axiosImproved()
+    .put(`https://devdeskqueue-be.herokuapp.com/api/tickets/${id}`, {
+      resolved,
+      assigned,
+      assigned_user,
+    })
+    .then(res => {
+      dispatch({ type: actionTypes.UPDATE_TICKET, payload: res.data });
+      return res;
+    })
+    .catch(err => {
+      console.log(err.message);
+      dispatch({ type: actionTypes.TICKET_ERROR });
+      return err;
+    });
+};
+
 export const getAdminTickets = id => dispatch => {
   dispatch({ type: actionTypes.GETTING_TICKETS });
 
