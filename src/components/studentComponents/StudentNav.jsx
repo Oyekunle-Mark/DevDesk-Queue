19097c28da3c74499c92c6e5a52a88ec0f8134fb
oyekunle-mark <oyekunlemac@gmail.com>
2019-06-26 15:53:40 +0100

    Improved the styling of the student nav link

--- src/components/studentComponents/StudentNav.jsx
+++ src/components/studentComponents/StudentNav.jsx
@@ -1,12 +1,67 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledStudentNav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  background: #101822;
+  padding: 7px 15px;
+  border-bottom: 1px solid #777777;
+
+  div {
+    display: flex;
+    align-items: center;
+  }
+
+  div img {
+    width: 50px;
+    height: 50px;
+  }
+
+  div h1 {
+    font-size: 20px;
+  }
+
+  div a {
+    margin: 0 10px;
+    font-size: 14px;
+  }
+
+  div a:hover {
+    text-decoration: underline;
+  }
+
+  .selected {
+    color: #b6b6b6;
+    font-weight: bold;
+  }
+`;
 
 const StudentNav = () => (
-  <nav>
-    <NavLink to="/home">Help Channel</NavLink>
-    <NavLink to="/create_ticket">Create Ticket</NavLink>
-    <NavLink to="/my_tickets">My Tickets</NavLink>
-  </nav>
+  <StyledStudentNav>
+    <Link to="/home">
+      <div>
+        <img src="../../assets/dev.svg" alt="logo" />
+
+        <h1>DevDesk</h1>
+      </div>
+    </Link>
+
+    <div>
+      <NavLink to="/home" activeClassName="selected">
+        Help Channel
+      </NavLink>
+      <NavLink to="/create_ticket" activeClassName="selected">
+        Create Ticket
+      </NavLink>
+      <NavLink to="/my_tickets" activeClassName="selected">
+        My Tickets
+      </NavLink>
+    </div>
+  </StyledStudentNav>
 );
 
 export default StudentNav;
