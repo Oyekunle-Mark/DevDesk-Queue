593ca50b3d8322cda191ef264a204eb6673f33c2
oyekunle-mark <oyekunlemac@gmail.com>
2019-06-26 13:39:44 +0100

    Styled the app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import styled from 'styled-components';
 
 import LandingPage from './components/LandingPage';
 import Registration from './components/Registration';
@@ -12,9 +13,25 @@ import HelperHome from './components/helperComponents/HelperHome';
 import ManageTickets from './components/helperComponents/ManageTickets';
 import NoMatchPage from './components/noMatchPage';
 
+const StyledApp = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 100%;
+  min-height: 100vh;
+  background: #161f2a;
+  color: #fdfdfd;
+  font-family: 'Montserrat', sans-serif;
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+`;
+
 const App = () => (
   <Router>
-    <div>
+    <StyledApp>
       <Switch>
         <Route exact path="/" component={LandingPage} />
         <Route
@@ -31,7 +48,7 @@ const App = () => (
         <WithAuth helper exact path="/manage_tickets" component={ManageTickets} />
         <Route component={NoMatchPage} />
       </Switch>
-    </div>
+    </StyledApp>
   </Router>
 );
 
