2154b3aeb59ba4bc39fd5a8724e1c35046866308
oyekunle-mark <oyekunlemac@gmail.com>
2019-06-26 13:40:15 +0100

    Applied styling to the header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,54 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  background: #101822;
+  padding: 7px 15px;
+
+  div {
+    display: flex;
+    align-items: center;
+  }
+
+  div img {
+    width: 50px;
+    height: 50px;
+  }
+
+  div h1 {
+    font-size: 20px;
+  }
+
+  div a {
+    margin: 0 10px;
+    font-size: 14px;
+  }
+
+  div a:hover {
+    text-decoration: underline;
+  }
+`;
 
 const Header = () => (
-  <header>
-    <h1>DevDesk Queue</h1>
+  <StyledHeader>
+    <Link to="/">
+      <div>
+        <img src="../../assets/dev.svg" alt="logo" />
+
+        <h1>DevDesk</h1>
+      </div>
+    </Link>
 
     <div>
       <Link to="/login">Login</Link>
       <Link to="/join">Register</Link>
     </div>
-  </header>
+  </StyledHeader>
 );
 
 export default Header;
