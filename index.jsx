8b0a5f2b139c97a0ed37f6837363788097f713c5
oyekunle-mark <oyekunlemac@gmail.com>
2019-06-28 17:44:41 +0100

    Removed the redux dev tools

--- index.jsx
+++ index.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { createStore, compose, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
 
 import App from './src/App';
 import rootReducer from './src/state/reducers';
@@ -11,11 +10,7 @@ import addTokenToLocalStorage from './src/state/middlewares';
 
 const store = createStore(
   rootReducer,
-  compose(
-    applyMiddleware(thunk, logger, addTokenToLocalStorage),
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__(),
-  ),
+  applyMiddleware(thunk, addTokenToLocalStorage),
 );
 
 render(
